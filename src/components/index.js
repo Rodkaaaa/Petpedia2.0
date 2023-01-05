2fa8884c109f3f91639748229908a07e0638cafb
Frederick Cid <frederickcid@gmail.com>
2023-01-04 21:55:19 -0300

    agregando secction

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Navbar from "./Navbar";
 import CarouselComponent from "./Carousel";
-export {Navbar, CarouselComponent}
+import SectionBody from "./SectionBody";
+export {Navbar, CarouselComponent, SectionBody}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { CarouselComponent, Navbar } from "../components";
+import { CarouselComponent, Navbar, SectionBody } from "../components";
+import "./Home.css";
 
 const Home = () => {
-    return(
-        <div>
-        <Navbar/>
-        
-        <CarouselComponent/>
-        </div>
-    )
-}
+  return (
+    <div className="all">
+      <Navbar />
+      <CarouselComponent />
+      <br />
+      <br />
 
-export default Home
+      <div className="completeSection">
+        <div className="texto">
+          <h2 className="tituloSection">Artículos de interés </h2>
+          <p className="textSection">
+            Los especialistas nos entregan todo su conocimiento
+          </p>
+        </div>
+        <div className="section ">
+          <SectionBody />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
